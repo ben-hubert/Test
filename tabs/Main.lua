3dbabcb8dbae9b69dec9532f194b47cbe496e8d0
ben-hubert at codea-scm <ben-hubert@codea-scm.aws.mapote.com>
2016-11-07 16:20:22 +0000

    Test

---
+++ tabs/Main.lua
@@ -0,0 +1,26 @@
+-- Test
+
+-- Use this function to perform your initial setup
+function setup()
+    print("Hello World!")
+end
+
+-- This function gets called once every frame
+function draw()
+    -- This sets a dark background color 
+    background(0, 0, 0, 255)
+    
+
+    -- This sets the line thickness
+    strokeWidth(5)
+
+    -- 
+    sprite("Documents:name of company", WIDTH/2, HEIGHT/2, WIDTH, HEIGHT)
+    
+    
+    
+    
+    
+end
+
+
